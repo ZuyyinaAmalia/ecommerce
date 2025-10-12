6b835fb279a9644ae19cd0b90e9fa8638bbf2081
Umar Faqih <umarfaqih@students.undip.ac.id>
2025-10-13 01:48:59 +0700

    coba

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import 'preline';
+import Swal from 'sweetalert2'
+
+window.Swal = Swal
+
+document.addEventListener('livewire:navigate', () => {
+    window.HSStaticMethods.autoInit();
+})
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\CheckoutPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\SuccessPage;
+use App\Livewire\CancelPage;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomePage::class);
+
+Route::middleware('auth')->group(function() {
+    Route::get('/logout', function() {
+        auth()->logout();
+        return redirect('/');
+    });
+    Route::get('/checkout', CheckoutPage::class);
+    Route::get('/my-orders', MyOrdersPage::class);
+    Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+    Route::get('/success', SuccessPage::class)->name('success');
+    Route::get('/cancel', CancelPage::class)->name('cancel');
 });
